745f281b511bf336a88eb58d9a4e3e2a6074bb3b
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-01 19:24:25 +0200

    display names from daja.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-// import Starter from './tutorial/01-useState/starter/01-error-example';
-import Starter from './tutorial/01-useState/starter/02-useState-basics'
+// import Starter from './tutorial/01-useState/starter/02-useState-basics'
+import Starter from './tutorial/01-useState/starter/03-useState-array'
+import Final from './tutorial/01-useState/final/03-useState-array'
 
 function App() {
   return (
     <div className='container'>
       <Starter/>
+
+      {/* <Final></Final> */}
     </div>
   );
 }
--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,18 @@
+import { data } from "../../../data";
+import React from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people,setPeople] = React.useState(data);
+
+  return (
+    <div>
+      {people.map((person) =>{
+            // console.log(person.name)
+            const {id,name} =person
+            return <div key={id}><h4>{name}</h4></div>
+      })}      
+    </div>
+  );
 };
 
 export default UseStateArray;
