dc19667702261518efbd9de0ef38194871e1bc40
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-01 21:32:36 +0200

    UseState object intro complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import Starter from './tutorial/01-useState/starter/02-useState-basics'
-import Starter from './tutorial/01-useState/starter/03-useState-array'
-import Final from './tutorial/01-useState/final/03-useState-array'
+// import Starter from './tutorial/01-useState/starter/03-useState-array'
+// import Final from './tutorial/01-useState/final/03-useState-array'
+import Starter from './tutorial/01-useState/starter/04-useState-object'
 
 function App() {
   return (
--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,24 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState("Peter");
+  const [age, setAge] = useState(25);
+  const [hobby, setHobby] = useState("Reading books");
+
+  const displayName =() =>{
+    setName('Chris')
+    setAge(45)
+    setHobby('Mahafhe')
+  }
+
+  return(
+  <>
+  <h2>useState object example</h2>
+  <h3>{name}</h3>
+  <h3>He is: {`${age} old`}</h3>
+  <h3>{hobby}</h3>
+  <button type="button" onClick={displayName}>Show Name</button>
+  </>)
 };
 
 export default UseStateObject;
