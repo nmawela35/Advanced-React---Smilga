c6cde992ea183ceaa998dd30040889fbc99ec7e4
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-02 09:27:09 +0200

    useState hook completed, now useEffect

--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -3,14 +3,25 @@ import { useState } from "react";
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
 
-  const handleClick = () => {
-/*     setValue((currentState) =>{
+/*   const handleClick = () => {
+    setValue((currentState) =>{
       const newState =currentState +1;
-      // console.log(newState)
+      console.log(newState)
       return newState;
     });
- */
-  };
+  }; */
+  const handleClick =() =>{
+    setTimeout(() =>{
+      console.log('Clicke the BTN')
+      setValue((currentState) =>{
+        const newState =currentState + 1;
+        return newState;
+      })
+    },3000)
+  }
+
+
+
   return(
   <div>
     <h1>{value}</h1>
