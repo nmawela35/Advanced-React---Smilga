68167bc6310a0295f7bdb7a694ed13f7886174ce
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-02 12:00:08 +0200

    useEffect multiple effects done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import Starter from './tutorial/02-useEffect/starter/01-code-example';
-import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics';
+// import Starter from './tutorial/02-useEffect/starter/02-useEffect-basics';
+import Starter from './tutorial/02-useEffect/starter/03-multiple-effects';
 function App() {
   return (
     <div className='container'>
