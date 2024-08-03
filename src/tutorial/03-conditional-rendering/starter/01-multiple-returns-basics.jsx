158843603989517ce15293e1e7d4e5975699acad
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-03 08:48:00 +0200

    multiple return basics lesson 1 done

--- src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
+++ src/tutorial/03-conditional-rendering/starter/01-multiple-returns-basics.jsx
@@ -1,6 +1,18 @@
 import { useEffect, useState } from 'react';
 
 const MultipleReturnsBasics = () => {
+  const [isLoading, setisLoading] =useState(true);
+  const [isError, setisError] =useState(true)
+
+  if (isLoading){
+    return <h2>Loading please wait...</h2>
+  }
+
+  if (isError){
+    return <h2>There was an error</h2>
+  }
+
   return <h2>Multiple Returns Basics</h2>;
 };
+
 export default MultipleReturnsBasics;
