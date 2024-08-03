510cdd7a98356b86c98eb861128e5dae2c39e1d3
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-03 10:51:15 +0200

    Hooks rule basics, now truthy/falsy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics';
-import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data';
+// import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics';
+// import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data';
+import Starter from './tutorial/03-conditional-rendering/starter/03-hooks-rule';
 
 function App() {
   return (
--- src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,6 +2,48 @@ import { useEffect, useState } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [isLoading,setisLoading] =useState(true);
+  const [isError,setisError] =useState(false);
+
+  const [user,isUser] =useState(null);
+
+  useEffect(() =>{
+    const fetchUser = async() =>{
+      try {
+
+        const response = await fetch(url);
+        if(!response.ok){
+          setisError(true)
+          setisLoading(false);
+          return;
+        }
+        const user = await response.json();
+          setUser(user)
+
+      } catch (error) {
+          setisError(true)
+          console.log(error)
+      }
+      setisLoading(false)
+    }
+    fetchUser();
+  },[])
+
+  if (isLoading){
+    return <h2>Loading please wait...</h2>
+  }
+
+  if (isError){
+    setisError(true)
+    return <h2>There was an error</h2>
+  }
+
+    const {avatar_url, name, company, bio} =user;
+  return <div>
+    <img src={avatar_url} alt={name} style = {{width:'150px', borderRadius: '25px'}} />
+    <h2>{name}</h2>
+    <h4>Works at {company}</h4>
+    <p>{bio}</p>
+  </div>;
 };
 export default MultipleReturnsFetchData;
