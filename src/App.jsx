d1a390f625e1a06063c4864c6765186a824c1801
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-03 15:00:11 +0200

    Short Circuit intro done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import Starter from './tutorial/03-conditional-rendering/starter/01-multiple-returns-basics';
 // import Starter from './tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data';
-import Starter from './tutorial/03-conditional-rendering/starter/03-hooks-rule';
+// import Starter from './tutorial/03-conditional-rendering/starter/03-hooks-rule';
+import Starter from './tutorial/03-conditional-rendering/starter/04-short-circuit-overview';
 
 function App() {
   return (
--- src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,18 @@
 import { useState } from 'react';
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  const [text,setText] =useState('');
+  const [name,setName] =useState('susan')
+
+  const codeExample =text || 'Hello World' 
+
+
+  return <div>
+    <h4>Falsy OR : {text || 'Hi Chris'}</h4>
+    <h4>Falsy AND : {text && 'Hi Chris'}</h4>
+    <h4>Truthy OR : {name || 'Hi Chris'}</h4>
+    <h4>Truthy OR : {name && 'Hi Chris'}</h4>
+    {codeExample}
+  </div>;
 };
 export default ShortCircuitOverview;
