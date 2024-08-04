0314966056420c38a7c124832aae985aa25f3a59
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-04 18:45:39 +0200

    Multiple inputs complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Forms from './tutorial/06-forms/starter/02-user-challenge'
+// import Forms from './tutorial/06-forms/starter/02-user-challenge'
+import Starter from './tutorial/06-forms/starter/03-multiple-inputs'
 
 function App() {
   return (
     <div className="container">
-      <Forms/>
+      <Starter/>
   
     </div>
   );
