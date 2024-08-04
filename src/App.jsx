fb629ecb2c3df2149b2f8d40a3b1de69678c39f6
Christopher Mawela <christopher.mawela@gmail.com>
2024-08-04 15:36:41 +0200

    Intro to Controlled Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import List from './tutorial/05-leverage-javascript/starter/List';
+import Forms from './tutorial/06-forms/starter/01-controlled-inputs'
 
 function App() {
   return (
     <div className="container">
-      <h2>Leverage JavaScript</h2>
-      <List/>
+      <Forms/>
   
     </div>
   );
--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+
 const ControlledInputs = () => {
-  return <h2>Controlled Inputs</h2>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleChange =(e)=>{
+    // console.log(e.target.name)
+    // console.log(e.target.value)
+    setName(e.target.value)
+    clg(name)
+  }
+  const handleSubmit =(e)=>{
+    e.preventDefault()
+    console.log(name, email)
+  }
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <h4>controlled inputs</h4>
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input type="text" className="form-input" id="name" value={name} onChange={handleChange} />
+      </div>
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input type="email" className="form-input" id="email"  value={email} onChange={(e) =>setEmail(e.target.value)}/>
+      </div>
+      <button type="submit" className="btn btn-block">
+        Submit
+      </button>
+    </form>
+  );
 };
 export default ControlledInputs;
